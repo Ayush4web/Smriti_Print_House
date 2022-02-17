e0019bcb3a8cb5c3fa88f853bcdff439fcb18971
Ayush <choubeyayush4@gmail.com>
2022-02-17 21:09:25 +0530

    work

--- submit.php
+++ submit.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "choubeyayush4@gmail.com"; // this is your Email address
+    $to = ""; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $subject = $_POST['subject'];
 	$name = $_POST['name'];
